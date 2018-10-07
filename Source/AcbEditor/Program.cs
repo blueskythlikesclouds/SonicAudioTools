39815c4c8ad259cd122ce80191498698e2d93556
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2018-10-07 14:48:06 +0300

    Fix case sensitivity

--- Source/AcbEditor/Program.cs
+++ Source/AcbEditor/Program.cs
@@ -33,7 +33,7 @@ namespace AcbEditor
             try
             {
 #endif
-            if (args[0].EndsWith(".acb"))
+            if (args[0].EndsWith(".acb", StringComparison.OrdinalIgnoreCase))
             {
                 var extractor = new DataExtractor();
                 extractor.ProgressChanged += OnProgressChanged;
